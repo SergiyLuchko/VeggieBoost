1b3624ff856d98c9a521b79fad5175b031180f41
SergiyLuchko <LuchkoSergiy85@gmail.com>
2025-01-20 18:54:11 +0200

    Initial commit

---
+++ src/js/components/slider.js
@@ -0,0 +1,6 @@
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+// Swiper.use([Navigation, Pagination]);
+// const swiper = new Swiper(el, {
+//   slidesPerView: 'auto',
+// });
